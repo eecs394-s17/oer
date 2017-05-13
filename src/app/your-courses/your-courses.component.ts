2363f2349fec91ae18697dc618719f433f1c62af
M. Smith <animaniac21@gmail.com>
2017-05-13 13:02:16 -0500

    our first http request

--- src/app/your-courses/your-courses.component.ts
+++ src/app/your-courses/your-courses.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-your-courses',
   templateUrl: './your-courses.component.html',
-  styleUrls: ['./your-courses.component.css']
+  styleUrls: ['./your-courses.component.css'],
+  providers: [UserService]
 })
 export class YourCoursesComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private userService: UserService) {}
   ngOnInit() {
-  }
 
+  }
+  profile = {};
+
+  loadUser() {
+    this.userService.getUser().subscribe(data => this.profile = data);
+  }
 }
