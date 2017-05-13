ac85d3ec19746df5b6b798f3fbf67c722aa5855d
Chris Chen <cschen13@gmail.com>
2017-05-13 18:26:50 -0500

    Add new course if not in database

--- src/services/course.service.ts
+++ src/services/course.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 
-const mockCourse = {
-  "id": 78818,
-  "title": "Music and Mind",
-  "term": "2010 Fall",
-  "instructor": "Richard D Ashley",
-  "subject": "MUS_THRY",
-  "catalog_num": "251-0",
-  "section": "20",
-  "room": "Music Administration Building 229",
-  "meeting_days": "MoWeFr",
-  "start_time": "09:00",
-  "end_time": "09:50",
-  "seats": 20,
-  "topic": null,
-  "component": "LEC",
-  "class_num": 16834,
-  "course_id": 19151
-};
+// const mockCourse = {
+//   "id": 78818,
+//   "title": "Music and Mind",
+//   "term": "2010 Fall",
+//   "instructor": "Richard D Ashley",
+//   "subject": "MUS_THRY",
+//   "catalog_num": "251-0",
+//   "section": "20",
+//   "room": "Music Administration Building 229",
+//   "meeting_days": "MoWeFr",
+//   "start_time": "09:00",
+//   "end_time": "09:50",
+//   "seats": 20,
+//   "topic": null,
+//   "component": "LEC",
+//   "class_num": 16834,
+//   "course_id": 19151
+// };
 
 @Injectable()
 export class CourseService {
@@ -55,6 +55,13 @@ export class CourseService {
     var textbook = this.db.list('/textbooks/').push({
       'title': title,
       'link': link
+    });
+    this.db.object('/courses/' + this.course.id).update({
+      'title': this.course.title,
+      'term': this.course.term,
+      'instructor': this.course.instructor,
+      'subject': this.course.subject,
+      'catalog_num': this.course.catalog_num
     });
     this.db.list('/courses/' + this.course.id + '/textbooks').push(textbook.key);
   }
