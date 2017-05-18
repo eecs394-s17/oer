c7981943b919b1b80d4d48a3603ca8fc8c1daafc
sameersrivastava <sameer.srivastava95@gmail.com>
2017-05-17 21:04:29 -0500

    Got search to work intially just for physics catalog_num

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { Subject } from 'rxjs/Subject';
+
 
 @Component({
   selector: 'app-search',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  items: FirebaseListObservable<any[]>;
+  departmentSubject: Subject<any>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(db: AngularFireDatabase) {
+    this.departmentSubject = new Subject();
+    this.items = db.list('/courses', {
+      query: {
+        orderByChild: 'catalog_num',
+        equalTo: this.departmentSubject
+      }
+    });
   }
+  ngOnInit(){
 
+  }
+  searching(){
+    console.log('searching');
+    this.departmentSubject.next("110-6"); 
+  }
+  
 }
