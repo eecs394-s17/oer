7ee21ba51d741b23c40a7691ae32c442ec883303
sameersrivastava <sameer.srivastava95@gmail.com>
2017-05-21 22:44:06 -0500

    Going to just pull all the data now, no need for a query.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
-import { Subject } from 'rxjs/Subject';
 
 
 @Component({
@@ -9,42 +8,18 @@ import { Subject } from 'rxjs/Subject';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  items: FirebaseListObservable<any[]>;
-  equalToProp: Subject<any>;
-  orderByChildProp: Subject<any>;
+  courses: FirebaseListObservable<any[]>;
   //search bar input
   stringText: string ='';
 
   ///selected option
-  selectedValue: string = '';
-  constructor(db: AngularFireDatabase) {
-    this.orderByChildProp = new Subject();
-    this.equalToProp = new Subject();
-
-      this.items = db.list('/courses', {
-        query: {
-          orderByChild: this.orderByChildProp,
-          equalTo: this.equalToProp
-          
-        }
-      });
+  constructor(private db: AngularFireDatabase) {
+    this.courses = db.list('courses/');
   }
   ngOnInit(){
 
   }
   searching(){
-    console.log("Searching for " + this.selectedValue + "s matching " + this.stringText);
-    switch (this.selectedValue) {
-      case 'Title':
-        this.orderByChildProp.next('title');
-        break;
-      case 'Course':
-        this.orderByChildProp.next('catalog_num');
-        break;
-      case 'Professor':
-        this.orderByChildProp.next('instructor');
-        break;
-    }
-    this.equalToProp.next(this.stringText); 
+    
   }
 }
