851f1bccb59bd9005c0a9d56e6c2c97c46c76948
Chris Chen <cschen13@gmail.com>
2017-05-26 22:51:35 -0500

    Remove textbook object when removing a textbook

--- src/services/course.service.ts
+++ src/services/course.service.ts
@@ -61,7 +61,12 @@ export class CourseService {
   }
 
   removeTextbookFromCourse(key: string) {
-    this.db.object('/courses/' + this.course.id + '/textbooks/' + key).set(null);
+    var textbookRef = this.firebaseApp.database().ref('/courses/' + this.course.id + '/textbooks/' + key);
+    textbookRef.once('value').then(function(snapshot) {
+      var textbookId = snapshot.val();
+      this.db.object('/textbooks/' + textbookId).remove();
+      this.db.object('/courses/' + this.course.id + '/textbooks/' + key).remove();
+    }.bind(this));
   }
 
   addTextbook(title: string, link: string, file: File, description: string) {
